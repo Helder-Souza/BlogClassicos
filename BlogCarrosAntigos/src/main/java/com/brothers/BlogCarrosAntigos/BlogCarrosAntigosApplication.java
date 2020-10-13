81b19870334e32a409c76b2250e314421a889269
unknown <helder825@hotmail.com>
2020-10-13 09:22:01 -0300

    Crud Project

---
+++ BlogCarrosAntigos/src/main/java/com/brothers/BlogCarrosAntigos/BlogCarrosAntigosApplication.java
@@ -0,0 +1,13 @@
+package com.brothers.BlogCarrosAntigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCarrosAntigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCarrosAntigosApplication.class, args);
+	}
+
+}
---
+++ BlogCarrosAntigos/src/main/java/com/brothers/BlogCarrosAntigos/repository/TemaRepository.java
@@ -0,0 +1,13 @@
+package com.brothers.BlogCarrosAntigos.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brothers.BlogCarrosAntigos.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	
+	public List<Tema>findAllByTemaContainingIgnoreCase(String tema);
+
+}
