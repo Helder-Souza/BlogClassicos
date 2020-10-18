402b9967f61a26769d64d7df054fe6a4863bdc46
unknown <valber_05@hotmail.com>
2020-10-18 14:30:40 -0300

    implements security

---
+++ BlogCarrosAntigos/src/main/java/com/brothers/BlogCarrosAntigos/repository/UsuarioRepository.java
@@ -0,0 +1,17 @@
+package com.brothers.BlogCarrosAntigos.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import com.brothers.BlogCarrosAntigos.model.Usuario;
+
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	public List<Usuario> findAllByNomeContainingIgnoreCase (String nome);
+	public Optional<Usuario> findByUsuarioAndSenha(String usuario, String senha);
+	public Optional<Usuario> findByUsuario(String usuario);
+}
